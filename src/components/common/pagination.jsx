46bf68830d3b643120068110d1dd00e5bfacebb4
MoActive <you@example.com>
2019-02-06 00:10:47 +0100

    pagination complete

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,9 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import _ from "lodash";
 
 const Pagination = props => {
   const { itemsCount, pageSize, onPageChange, currentPage } = props;
-  console.log(currentPage);
   const pageCount = Math.ceil(itemsCount / pageSize);
   if (pageCount === 1) return null;
   const pages = _.range(1, pageCount + 1);
